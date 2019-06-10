58178d099ad5c46557cdb648e4c95c114e291995
Keven <kevenmolina@gmail.com>
2019-06-10 17:26:18 -0500

    finished creating bird model

--- main_app/models.py
+++ main_app/models.py
@@ -4,3 +4,5 @@ from django.db import models
 class Bird(models.Model):
     name= models.CharField(max_length= 100)
     species= models.CharField(max_length = 100)
+    description= models.CharField(max_length= 100)
+    age = models.IntegerField("How old is he/her?")
